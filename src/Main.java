64473e35f298231f1eb46a40d50834b6a6904c6b
sergeydomanov <nomad-labs@yandex.ru>
2022-08-22 21:03:05 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        if (checkingYear() % 4 == 0 && checkingYear() % 100 != 0 || (checkingYear() % 400 == 0))
+        System.out.println(checkingYear() + " - високосный год");
+        else System.out.println (checkingYear() + " - не високосный год");
     }
+
+    static int checkingYear() {
+        int currentYear = 2020;
+        return currentYear;
+    }
+
+
 }
+
